499179c14bd8c30f90410fcde263b5c5eb72f371
Yamuna.D <Yamuna.D@EXIND3010-40.allfon.in>
2022-03-21 12:49:10 +0530

    req-git to commit

--- jenkins-maven-git-docker_build/src/main/java/com/example/demo/controller/TeacherController.java
+++ jenkins-maven-git-docker_build/src/main/java/com/example/demo/controller/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,15 @@ public class TeacherController {
 	return list.toString();
 	}
 
-
+	@GetMapping(path="/teachers/{dept}")
+	public String getTeacherByDept(@PathVariable("dept") String dept) {
+	List<String> list = Arrays.asList("amar","swetha","sumi");
+	
+	if(dept.equals("cse")) {
+		list = Arrays.asList("amar","swetha","sumi");		
+	}
+	return list.toString();
+	}
 }
 
 
